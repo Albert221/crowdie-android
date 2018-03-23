4e9ff0f1a95b4d7358f0e5799f9e798bbd624417
Albert221 <w.albert221@gmail.com>
2018-03-23 23:06:53 +0100

    Initial commit

---
+++ app/src/main/kotlin/wolszon/me/groupie/MainActivity.kt
@@ -0,0 +1,12 @@
+package wolszon.me.groupie
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
